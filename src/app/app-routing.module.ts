9dad6e645a58ea27078cbc2ff8a53804d528b90d
sakis <manowarlik@gmail.com>
2018-12-16 23:33:22 +0200

    github pages build commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,10 +7,10 @@ import { TestComponent } from './test/test.component';
 import { Test2Component } from './test2/test2.component';
 
 const routes: Routes = [
- /*  { path: '', redirectTo: 'ImgAnalysisToolkit', pathMatch: 'full' }, */
+  { path: '', redirectTo: 'ImgAnalysisToolkit', pathMatch: 'full' },
   { path: 'TextSelection', component: CropperComponent},
-  /* { path: 'ImgAnalysisToolkit', component: TabsComponent}, */
-  { path: '', component: TabsComponent},
+  { path: 'ImgAnalysisToolkit', component: TabsComponent},
+  //{ path: '', component: TabsComponent},
   { path: 'about', component: AboutComponent},
   { path: 'test', component: TestComponent},
   { path: 'test2', component: Test2Component}
